eb87765fa03bb4dfefbaf191ca2abd210f810ad6
matteoSonniez <matteo.sonniez@ynov.com>
2023-12-07 16:10:12 +0100

    image_to_s3_v1

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const multer = require('multer');
 const router = express.Router();
 const productController = require('../controllers/product.controller');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.get('/:id', productController.getProduct);
 router.get('/', productController.getProducts);
+router.post('/create', upload.single('image'), productController.createProduct);
 
 module.exports = router;
