f5f2ee7588cbc52b9c4b475d5fe4a8283f4c55ee
matteoSonniez <83650272+matteoSonniez@users.noreply.github.com>
2023-12-07 16:13:46 +0100

    image_to_s3_v1 (#2)

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const config = {
     port: process.env.PORT,
     frontend_url: process.env.FRONTEND_URL,
+    access_Key: process.env.ACCESS_KEY,
+    secret_access_Key: process.env.SECRET_ACCESS_KEY,
 };
 
 module.exports = config;
